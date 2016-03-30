28cd716e39b92e7cec9cc186a0e309b28bf04903
Kevin <kkornafel82@yahoo.com>
2016-03-29 20:44:01 -0700

    user story 2 completed

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,26 +1,55 @@
 (function() {
    function LandingCtrl($scope, $interval) {   
-      $scope.counter = 1500;
+      $scope.counter = 10;
       $scope.runningInterval = null;
+      $scope.onBreak = false;
 
       $scope.onTimeout = function(){
         if ($scope.counter > 0) {
             $scope.counter = $scope.counter-1;
         }
-        else {
-            alert("Time is up!");
+        else if ($scope.counter < 1) {
+            $scope.runningInterval = null;
+            alert("Take a break!");
+            $scope.onBreak = true;
+            $scope.counter = 5;
+        }
+      };
+
+      $scope.newBreak = function(){
+          if ($scope.counter > 0) {
+            $scope.counter = $scope.counter-1;
+          }
+          else if ($scope.counter < 1) {
+            alert("Back to work!");
+            $scope.onBreak = false;
+            $scope.counter = 10;
+            $scope.runningInterval = null;
         }
       };
 
       $scope.myTimeout = function() {
-        $scope.runningInterval = $interval($scope.onTimeout,1000, $scope.counter, true);
+        $scope.runningInterval = $interval($scope.onTimeout,1000, $scope.counter+1, true);
       };
+
+      $scope.myBreak = function() {
+        $scope.runningInterval = $interval($scope.newBreak,1000, $scope.counter+1, true);
+      };     
     
       $scope.reset= function(){
         $interval.cancel($scope.runningInterval);
-        $scope.counter = 1500;
+        
+        if ($scope.onBreak) {
+          $scope.counter = 5;
+        }else {
+          $scope.counter = 10;
+        };
+
         $scope.runningInterval = null;
-      };  
+        
+      };
+
+
 }
 
 
