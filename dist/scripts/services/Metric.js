37439fc50fbc2a07a46b35f63c717ba958abcd82
Kevin <kkornafel82@yahoo.com>
2016-04-24 20:46:04 -0700

    attempt to fix metric.js

--- dist/scripts/services/Metric.js
+++ dist/scripts/services/Metric.js
@@ -1,27 +1,37 @@
 (function() {
-  function Metric($rootScope) {
-    $rootScope.workSessions = [];
+  function Metric($firebaseObject) {
+    
+    workSessions = [];
+    
+    var ref = new Firebase("https://scorching-heat-2362.firebaseio.com/analytics");
+    var analytics = $firebaseObject(ref);
+    window.analytics = analytics;
+    
 
     return {
-      // Function that records a metric object by pushing it to the $rootScope array
-      registerWorkSession: function() {
-        // Add time to event register
-        var session = new Date();
-        $rootScope.workSessions.push(session);
-      },
-      numberOfSessions: function() {
-        var sessions = [];
-        angular.forEach($rootScope.workSessions, function() {
-            sessions.push("1");
-        });
-        return sessions.length;
-      }
+      
+      registerPageView: function() { 
+      analytics.$loaded().then(function(){
+        console.log("It worked");
+        analytics.pageViews++;
+        analytics.$save();
 
+      }) 
+      },
+
+      registerWorkSessionStart: function() {
+        var session = new Date();
+        workSessions.push(session);
+      },
+
+      numberOfSessionsStarted: function() {
+        return workSessions.length;
+      }
 
     };
   }
 
   angular
     .module('blocTime')
-    .factory('Metric', ['$rootScope', Metric]);
+    .factory('Metric', ['$firebaseObject', Metric]);
 })();
